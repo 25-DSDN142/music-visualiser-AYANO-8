0bee99e78b08ff508be9e9bedaa5d5ff8ce8614e
AYANO-8 <ayanodesu0995@gmail.com>
2025-09-07 18:01:23 +1200

    Create music_try.js

---
+++ music_try.js
@@ -0,0 +1,33 @@
+let stage = 0;
+let stageStarttime = 0;
+let circleAngle = 0;
+
+let snail1= [];
+let snail2 =[];
+let fallingCircles = [];
+let triangleColor = [];
+
+function setup() {
+    createCanvas(600, 800);
+
+ snail1.push(loadImage('snail_0.png'));
+ snail2.push(loadImage('snai2_0.png'));
+ snail3.push(loadImage('snai3_0.png'));
+ snail4.push(loadImage('snai4_0.png'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+}
