a93f481b07c612b9f6ec9f4957ddc35f6af88374
AYANO-8 <ayanodesu0995@gmail.com>
2025-09-08 21:02:47 +1200

    Update music_try.js

--- music_try.js
+++ music_try.js
@@ -10,6 +10,10 @@ let triangleColor=[];
 const unmtriangles =12;
 const numcircles =20;
 
+let ookisa1 =0;
+let ookisa2 =200;
+let ookisa3 =400;
+
 
 function setup() {
     createCanvas(600, 800);
@@ -60,33 +64,102 @@ function draw_one_frame(vocal, drum, bass, other){
     ImageMode (CENTER);
 
     if (stage ==0){
-        Image snailimage
+        drawStage0(image, elapsed);
+    }else if (stage ==1) {
+        drawStage1(image, vocal, drum, elapsed);
+    } else if (stage ==2) {
+        drawStage2(image, bass, other, elapsed);
+
+    }
+    }
+
+    //ステージ０の状態
+    founction drawStage0 (image, elapsed) {
+        image(snail1Images [image], wigth/2, height/2, 200, 200);
+        if (elapsed >1000) {
+            stage =1;
+            stageStartTime =millis();
+
+        }
+    }
+
+//ステージ１の状態
+function drawStage1(image, vocal, drum, elapsed) {
+    image(snail2image [image], width/2, height /2, 200, 200);
+     
+    drawCenterCircle(vocal);
+    drawRotatingtriangles(drum);
+
+    if (elapsed >120000) {
+        stage =2;
+        stageStartTime =millis();
+
     }
 }
+
+//ステージ２
+function drawStage2 (image, bass, other, elapsed) {
+    image(snail2Images [image], width/2, height/2, 200, 200);
+    drawfallingCircles (other, bass);
+    if (elapsed >60000) noLoop();
+
+}
+
+
+let circlrSize =map(vocal, ookisa1, 100, 50, 200);
+noStroke();
+fill (255, 200, 0);
+ellipse(width/2, height/2, circleSize);
+
 
 
 push();
 translate8width/2, heighr/2);
 let triangleRadius =map(drum, 0, 100, circleSize/2)
 
+for (let i =0; i<numTriangles; i++) {
+    let angle = circleAngle + (360 / numTriangles)* i;
 
+    //三角形の中心座標
+    let x = cos(angle) * triangleRadius;
+    let y =sin (angle) * triangleRadius;
 
+    fill (trianglecolor[i]);
 
-
-
-
-
-
-for (let c of fallingCircles){
-    let circleSize =map(other, 0, 100, c.baseSize/2, c.baseSize/2);
-    FileList(c.color);
-    ellipse
+    push();
+    translate(x, y);
+    rotate (angle);
+    triang (0, -10, -10, 10, 10, 10);
+    pop();
 }
- )
+pop();
+
+circlrAngle +=2;
 
 
 
 
+
+
+
+
+image (snail2Images[image],width/2, height/2, 200, 200)
+
+fill(255, 30, 43);
+ellipse(150, ookisa, bass, bass);
+ookisa +=10;
+if (ookisa1 >height) ookisa =0;
+
+fill(255, 50, 100);
+ellipse(300, ookisa2, bass, bass);
+ookisa2 +=8;
+if (ookisa2 >height)ookisa2 =0;
+
+fill(255, 200, 50);
+ellipse(450, ookisa3, bass, bass);
+ookisa3 +=12;
+if (ookisa3>height) ookisa3 =0;
+}
 
 
 
