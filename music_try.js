9b0195c3fe56c3895a35cfb69bf293aad28b8bd3
AYANO-8 <ayanodesu0995@gmail.com>
2025-09-09 22:22:06 +1200

    Update music_try.js

--- music_try.js
+++ music_try.js
@@ -1,28 +1,85 @@
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(255, 25,0 )
   textFont('Verdana'); // please use CSS safe fonts
   
   textSize(24);
 
   
+rectMode(CENTER);
 
- stroke(255);
+ noStroke();
+fill (210, 180, 140);  
+ quad(405, 800, 135, 800, 0,960,540,960);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ stroke(75, 54, 33);
  strokeWeight(4);
- fill(0);
- rect(30, 390,100, 200);
+ fill(101,67,33);
+rect(80, 800, 140, 200);
+rect(460, 800, 140, 200);
+
+rect(80, 600, 140, 200);
+rect(460, 600, 140, 200);
+
+
 
 
 
  noStroke();
-fill (255,25,0);  
- quad(405, 650, 135, 650, 0,780,540,780);
+ fill(59, 47, 47);
+ rect(80, 750, vocal, 50);//左
+ rect(460, 750, vocal, 50);//右
 
 
- fill(128);
-ellipse(80, 450, vocal, vocal);
+ellipse(30, 520, 10, 10);
+ellipse(130, 520, 10, 10);
+ellipse(30, 680, 10, 10);
+ellipse(130, 680, 10,10);
 
-ellipse(80, 550, other, other);
 
+
+
+
+
+
+ellipse(80, 840, drum, drum);
+ellipse(460, 840, drum, drum);
+
+ellipse(80, 640, bass, bass);
+rect(460, 550, other, 50);
+rect(80, 550, other, 50);
+ellipse(460, 640, bass, bass);
+
+/*rect(80, 802, 20, 200);
+rect(460, 802, 20, 200);
+
+stroke(128);
+strokeWeight(7);
+line (70, 850, 40, 950);
+line(90, 850, 120, 950);
+
+line(470, 850, 500, 950);
+line(450, 850, 420, 950);*/
+
+ 
 
 
 }
