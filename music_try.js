149550086c8f7124dd0fef5349c21657cae87a4c
AYANO-8 <ayanodesu0995@gmail.com>
2025-09-13 16:14:56 +1200

    Update music_try.js

--- music_try.js
+++ music_try.js
@@ -2,7 +2,6 @@ let pic =[];
 let imagesloaded = false;
 let stars = [];
 let numberofstars = 500;
-
 let starsInitialized = false;
 let shootingstars = [];
 let shootingstarsgap = 30;
@@ -41,33 +40,31 @@ rectMode(CENTER);
 
  //images setting
 let VocalFrame = int(map(vocal, 0, 100, 0, 3));
- VocalFrame = constrain(VocalFrame, 0, 2);
-if( vocal < 1) {
+ //VocalFrame = constrain(VocalFrame, 0, 2);
+ //ratios
+if( vocal < 20) {
   VocalFrame = 0;
-} else if (vocal < 70) {
+} else if (vocal < 72) {
   VocalFrame = 1;
 } else {
   VocalFrame = 2;
 } 
 
 
+
+//the audience image
 let BassFrame = int(map(bass, 0, 100, 0, 3));
-BassFrame = constrain(BassFrame, 0, 3);
+//BassFrame = constrain(BassFrame, 0, 3);
+//ratios
 if( bass < 70) {
   BassFrame = 0;
-} else if (bass < 90) {
+} else if (bass < 79) {
   BassFrame = 1;
 } else if (bass < 100) {
   BassFrame = 2;
 } else {
   BassFrame = 3;
 }
-
-
-
-
-
-
 
 
  //stars setting
@@ -105,56 +102,49 @@ if (m.y > 540 || m.x > 800) {
 }
 
 
-
-
 //stage
 noStroke();
 fill (234, 34, 100);  
-quad(500, 350, 300, 350, 0,430,800,430);
-fill(75, 0, 130);
-rect(400,490, 800, 120);
+quad(500, 350, 300, 350, 0,450,800,450);
+fill(52, 76, 183);
+rect(400,510, 800, 120);
 
 //speakers
 stroke(0);
 strokeWeight(4);
-fill(0, 9, 87);
-rect(100, 320, 140, 200);
-rect(700, 320, 140, 200);
-
-
+fill(75, 0, 130);
+rect(100, 340, 140, 200);
+rect(700, 340, 140, 200);
 
 noStroke();
-fill(52, 76, 183);
-rect(100, 270, other, 50);//left speaker
-rect(700, 270, other, 50);//right speaker
-ellipse(100, 350, drum, drum);//left speaker
-ellipse(700, 350, drum, drum);//right speaker
+fill(30, 9, 87);
+rect(100, 280, other, 50);//left speaker
+rect(700, 280, other, 50);//right speaker
+ellipse(100, 370, drum, drum);//left speaker
+ellipse(700, 370, drum, drum);//right speaker
 
 
 //circle around the speaker(left)
-ellipse(45, 240, 10, 10);
-ellipse(155, 240, 10, 10);
-ellipse(45, 300, 10, 10);
-ellipse(155, 300, 10,10);
-ellipse(45, 400, 10, 10);
-ellipse(155, 400, 10, 10);
+ellipse(45, 260, 10, 10);
+ellipse(155, 260, 10, 10);
+ellipse(45, 320, 10, 10);
+ellipse(155, 320, 10,10);
+ellipse(45, 420, 10, 10);
+ellipse(155, 420, 10, 10);
 
 //circle around the speaker(right)
-ellipse(645, 240, 10, 10);
-ellipse(755, 240, 10, 10);
-ellipse(645, 300, 10, 10);
-ellipse(755, 300, 10, 10);
-ellipse(645, 400, 10, 10);
-ellipse(755, 400, 10, 10);
+ellipse(645, 260, 10, 10);
+ellipse(755, 260, 10, 10);
+ellipse(645, 320, 10, 10);
+ellipse(755, 320, 10, 10);
+ellipse(645, 420, 10, 10);
+ellipse(755, 420, 10, 10);
 
-//images
+//hedgehog images
 push();
 scale (0.2);
-image(pic[VocalFrame],1500, 550);
+image(pic[VocalFrame],1500, 580);
 pop ();
-
-
-
 
 //moon
 fill(255, 235, 0); //yellow
@@ -164,26 +154,28 @@ ellipse(650, 120, 70, 70);
 
 //mike
 fill(169,169, 169);
-ellipse(420, 250, 15, 15);
-rect(430, 325, 5, 150);
+rect(430, 335, 5, 150);
 fill(105, 105, 105);
-rect(430, 250, 20, 5);
+rect(430, 260, 20, 5);
+fill(169,169, 169);
+ellipse(420, 260, 15, 15);
 
 //mike stand
 stroke(169,169, 169);
 strokeWeight(2);
-line (430,370, 420, 405);
-line (430, 370, 440, 405);
+line (430,360, 420, 405);
+line (430, 360, 440, 405);
 stroke(0);
-line (420, 244, 420, 257);
-line (415, 246, 415, 255);
-line (425, 246, 425, 255);
-line (414, 248, 427, 248);
-line (414, 253, 427, 253);
+line (420, 254, 420, 267);
+line (415, 256, 415, 265);
+line (425, 256, 425, 265);
+line (414, 258, 427, 258);
+line (414, 263, 427, 263);
 
+//the audience image
 push();
 scale(0.2);
-image(pic[3 + BassFrame], 100, 350);
+image(pic[3 + BassFrame], 100, 300);
 pop()
 
 }
